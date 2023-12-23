dbf4383dc0840ffae667eea09b55fede3fdc90da
Sahil Rajput <rajputsahil.2204@gmail.com>
2023-12-23 14:55:54 +0530

    Handle Yes/No Button Submit Event : to change text and gif using useState Hook : New Logic Added

--- src/AskContent/AskContent.js
+++ src/AskContent/AskContent.js
@@ -2,21 +2,32 @@ import React from "react";
 import "./AskContent.css"
 
 const AskContent = () => {
+    const [yesClick, setYesClick] = React.useState(false);
+
     return (
         <React.Fragment>
             <div className="container">
-                <div id="mainText">
+                {!yesClick && <div id="mainText">
                     <p>Do you wanna go out with me?</p>
+                </div>}
+                {yesClick && <div id="mainText">
+                    <p>
+                        Yeeeyyyy, Can't wait to meet you !!
+                    </p>
                 </div>
-                <div id="askGif">
+                }
+                {!yesClick && <div id="askGif">
                     <img src="https://media.giphy.com/media/cLS1cfxvGOPVpf9g3y/giphy.gif" alt="Ask Gif" />
-                </div>
+                </div>}
+                {yesClick && <div id="askGif">
+                    <img src="https://media.giphy.com/media/T86i6yDyOYz7J6dPhf/giphy.gif" alt="Yes Gif" />
+                </div>}
                 <div className="btns">
-                    <button className="btn">Yes</button>
-                    <button className="btn">No</button>
+                    <button className="btn" onClick={() => setYesClick(true)}>Yes</button>
+                    <button className="btn" onClick={() => setYesClick(false)}>No</button>
                 </div>
             </div>
-        </React.Fragment>
+        </React.Fragment >
     );
 }
 
